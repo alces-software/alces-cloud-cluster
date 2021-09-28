6b2ae46969d67ce7403f0fba0c99c47e228ea8ff
Stu Franks <stu@stuts.uk>
2021-09-28 13:17:31 +0100

    Initial commit of gateway setup script

---
+++ scripts/cgw1.sh
@@ -0,0 +1,43 @@
+yum -y update 
+
+cat << "EOF" > /etc/profile.d/flightcenter.sh
+#Custom PS1 with client name
+[ -f /etc/flightcentersupported ] && c=32 || c=31
+if [ "$PS1" ]; then
+  PS1="[\u@\h\[\e[1;${c}m\] [clustername-cloud1]\[\e[0m\] \W]\\$ "
+fi
+EOF
+
+touch /etc/flightcentersupported
+
+hostnamectl set-hostname cgw1.cloud1.pri.clustername.alces.network
+
+# HTTP Server
+yum -y install httpd
+
+cat << EOF > /etc/httpd/conf.d/deployment.conf
+<Directory /opt/flight/deployment/>
+    Options Indexes MultiViews FollowSymlinks
+    AllowOverride None
+    Require all granted
+    Order Allow,Deny
+    Allow from 10.110.0.0/16
+</Directory>
+Alias /deployment /opt/flight/deployment/
+EOF
+
+systemctl enable httpd
+systemctl start httpd
+
+# Install Azure CLI
+## rpm --import https://packages.microsoft.com/keys/microsoft.asc
+## cat << EOF > /etc/yum.repos.d/azure-cli.repo
+## [azure-cli]
+## name=Azure CLI
+## baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+## enabled=1
+## gpgcheck=1
+## gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+## EOF
+## yum install azure-cli
+
