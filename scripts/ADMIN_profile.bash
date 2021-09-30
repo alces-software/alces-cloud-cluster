f18d6c0deffcad2bb7bddcd16105cd0b72ed5048
Stu Franks <stu@stuts.uk>
2021-09-30 14:38:00 +0100

    Ensure log file appended to

--- scripts/ADMIN_profile.bash
+++ scripts/ADMIN_profile.bash
@@ -1,8 +1,8 @@
-curl http://10.110.1.200/deployment/scripts/ALL01-site.bash  | /bin/bash -x | tee /tmp/mainscript-default-output
+curl http://10.110.1.200/deployment/scripts/ALL01-site.bash  | /bin/bash -x | tee -a /tmp/mainscript-default-output
 curl http://10.110.1.200/deployment/scripts/AZURE01-base.bash  | /bin/bash -x | tee -a /tmp/mainscript-default-output
 curl http://10.110.1.200/deployment/scripts/AZURE02-tmpdisk.bash  | /bin/bash -x | tee -a /tmp/mainscript-default-output
-curl http://10.110.1.200/deployment/scripts/SLAVE01-base.bash | /bin/bash -x | tee /tmp/mainscript-default-output
-curl http://10.110.1.200/deployment/scripts/SLAVE99-nfs.bash | /bin/bash -x | tee /tmp/mainscript-default-output
-curl http://10.110.1.200/deployment/scripts/HPC01-base.bash | /bin/bash -x | tee /tmp/mainscript-default-output
-curl http://10.110.1.200/deployment/scripts/HPC02-pkgs.bash | /bin/bash -x | tee /tmp/mainscript-default-output
-curl http://10.110.1.200/deployment/scripts/SLAVE90-ipa.bash | /bin/bash -x | tee /tmp/mainscript-default-output
+curl http://10.110.1.200/deployment/scripts/SLAVE01-base.bash | /bin/bash -x | tee -a /tmp/mainscript-default-output
+curl http://10.110.1.200/deployment/scripts/SLAVE99-nfs.bash | /bin/bash -x | tee -a /tmp/mainscript-default-output
+curl http://10.110.1.200/deployment/scripts/HPC01-base.bash | /bin/bash -x | tee -a /tmp/mainscript-default-output
+curl http://10.110.1.200/deployment/scripts/HPC02-pkgs.bash | /bin/bash -x | tee -a /tmp/mainscript-default-output
+curl http://10.110.1.200/deployment/scripts/SLAVE90-ipa.bash | /bin/bash -x | tee -a /tmp/mainscript-default-output
