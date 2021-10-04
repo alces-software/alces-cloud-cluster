53bf36b1f1ff4337166d770d539ad624a97c8a84
Stu Franks <stu@stuts.uk>
2021-10-04 15:05:34 +0100

    Add Flight Setup script for salt stack

--- scripts/ALL03-fcops.bash
+++ scripts/ALL03-fcops.bash
@@ -1,2 +1,16 @@
 # Requires on-site VPN to be setup
 curl http://10.10.0.2/resources/salt/salt_minion.sh | bash
+
+cat << EOF > /var/lib/flight-setup/scripts/03-fcops.bash
+MAX=24 # ~2 minute of waiting for mount
+COUNT=1
+while [ ! -f /tmp/fcops_adopted ] ; do
+    echo "FCOPs salt completion check loop #$COUNT"
+    sleep 5
+    if [ $COUNT -eq $MAX ] ; then
+        scontrol update NodeName=$(hostname -s) State=DRAIN Reason="Ops configuration not complete"
+        break
+    fi
+    COUNT=$((COUNT + 1))
+done
+EOF
